ed97ce338d1bc9658d30b670ac62e94979af71b4
omarhegazy <omarbicboi@gmail.com>
2018-09-15 10:17:24 -0400

    finally fixed

--- dreidelGameOmar.sol
+++ dreidelGameOmar.sol
@@ -46,52 +46,57 @@ contract Dreidel {
 
     }
 
-    function computeGame() public view returns(uint8[]) {
+    function computeGame() public view returns(uint8[], uint8, uint8) {
         //come on Barbie let's go party
         require(playerCount > 1);
+        uint8 rounds = 0;
         uint8[] memory pieceCount = new uint8[](playerCount);
         uint8 pot = 0;
         for (uint i = 0; i < playerCount; i++)  pieceCount[i] = PIECE_AMT;
-        bytes32 game = generateRandomNumber();
+        uint256 game = generateRandomNumber();
         uint8 playerIndex = 0;
         uint8 livingPlayers = playerCount;
-        uint8 bytesIndex = 0;
-        bytes1 three = bytes1(3*16);
-        while (livingPlayers > 1 && bytesIndex < 32) {
+        while (livingPlayers > 1 && game >= 4) {
             while(pieceCount[playerIndex] == 0) 
                 playerIndex = (playerIndex + 1) % uint8(playerCount);
-            bytes1 curr = game[bytesIndex];
-            for(uint8 j = 0; j < 4; j++) {
-                bytes1 ourTurn = (curr & three);
-                curr = curr << 2;
+                uint256 ourTurn = game % 4; 
                 if (ourTurn == 0) { //nun
                     playerIndex = (playerIndex + 1) % uint8(playerCount); 
                 } else if (ourTurn == 1) { //gimel
                     pieceCount[playerIndex] += pot;
+                    pot = 0;
                     playerIndex = (playerIndex + 1) % uint8(playerCount);
                 } else if (ourTurn == 2) { //hei
-                    pieceCount[playerIndex] += (pot + (pot % 2)) / 2;
+                    uint8 takeAway = (pot + (pot % 2)) / 2;
+                    pieceCount[playerIndex] += takeAway;
+                    pot -= takeAway;
                 } else if (ourTurn == 3) { //shin
-                    uint8 remove = 0;
                     for (uint8 k = 0; k < 2 && pieceCount[playerIndex] > 0; k++) {
-                        remove++; pieceCount[playerIndex]--;
+                        pot++; pieceCount[playerIndex]--;
                     }
-                    pot+= remove;
                 }
+            for(uint8 j = 0; j < playerCount; j++) {
+                if (pieceCount[j] == 0) {
+                    livingPlayers--;
+                    continue;
+                }
+                pot += 1;
+                pieceCount[j] -= 1;
             }
-            bytesIndex++;
+            game = game >> 2;
+            rounds++;
         }
-        return pieceCount;
+        return (pieceCount, pot, rounds);
     }
     
-    function generateRandomNumber() private view returns (bytes32) {
+    function generateRandomNumber() public view returns (uint256) {
         //DEEPLY unsafe - change before production
-        return keccak256(block.timestamp);
+        return uint256(keccak256(block.timestamp));
     }
     
-    function rightShift() public view returns (bytes1, bytes1, bytes1) {
-        bytes32 test = generateRandomNumber();
-        return (test[0], bytes1(3*16), bytes1(test[0]) & bytes1(3*16));
+    function rightShift() public view returns (uint256, uint256, uint256) {
+        uint256 test = generateRandomNumber();
+        return (test, uint256(4), test % 4);
     }
     function kill() private { 
         if (msg.sender == owner) 
