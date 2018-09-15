1fd85fb49a4a2d713ed1f857369a98cb493b5b2a
omarhegazy <omarbicboi@gmail.com>
2018-09-15 08:35:26 -0400

    uhhhh.... core game logic?

--- dreidelGameOmar.sol
+++ dreidelGameOmar.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.4.22;
-import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
-contract Dreidel is usingOraclize {
+contract Dreidel {
     address public owner; //contract owner
     address[6] public players; //players in the game
     uint8 public playersUpper; //max players before game auto-starts
@@ -11,6 +10,7 @@ contract Dreidel is usingOraclize {
     uint public endTime;
     uint8 public MIN_PLAYERS = 2; //constant : number of players n
     uint8 public PIECE_AMT = 10;
+    uint public STAKE_WORTH;
     uint8 public TIMEOUT = 240; //time elapsed before timeout
     uint public ONE_ETH = 1000000000000000000;
 
@@ -30,30 +30,65 @@ contract Dreidel is usingOraclize {
         }
 
         startTime = block.number;
-
+        STAKE_WORTH = stake / PIECE_AMT;
         // Executer incentive to follow; watchdog for timed
         // kill() by startTime + TIMEOUT
     }
 
 
     function join() payable public{
-        require(msg.value != stake);
-        require(playerCount + 1 > playersUpper);
+        require(msg.value == stake);
+        require(playerCount + 1 <= playersUpper);
         playerCount++;
         players[playerCount - 1] = msg.sender;
         //if (players == playersUpper) //tell owner to cancel watchdog & start game
 
     }
 
-    function computeGame() public {
+    function computeGame() public view returns(uint8[]) {
         //come on Barbie let's go party
-        mapping(address => uint8) player
-
+        require(playerCount > 1);
+        uint8[] storage pieceCount;
+        uint8 pot = 0;
+        for (uint i = 0; i < playerCount; i++)  pieceCount.push(PIECE_AMT);
+        bytes32 game = generateRandomNumber();
+        uint8 playerIndex = 0;
+        uint8 bytesIndex = 0;
+        bytes1 three = bytes1(3*16);
+        while (pieceCount.length > 1 && bytesIndex < 32) {
+            bytes1 curr = game[bytesIndex];
+            for(uint8 j = 0; j < 4; j++) {
+                bytes1 ourTurn = (curr & three);
+                curr = curr << 2;
+                if (ourTurn == 0) { //nun
+                    playerIndex = (playerIndex + 1) % uint8(pieceCount.length); 
+                } else if (ourTurn == 1) { //gimel
+                    pieceCount[playerIndex] += pot;
+                    playerIndex = (playerIndex + 1) % uint8(pieceCount.length);
+                } else if (ourTurn == 2) { //hei
+                    pieceCount[playerIndex] += (pot + (pot % 2)) / 2;
+                } else if (ourTurn == 3) { //shin
+                    uint8 remove = 0;
+                    for (uint8 k = 0; k < 2 && pieceCount[playerIndex] > 0; k++) {
+                        remove++; pieceCount[playerIndex]--;
+                    }
+                    pot+= remove;
+                    if (pieceCount[playerIndex] == 0) delete pieceCount[playerIndex];
+                }
+            }
+            bytesIndex++;
+        }
+        return pieceCount;
     }
     
-    function generateRandomNumber() public returns uint {
+    function generateRandomNumber() private view returns (bytes32) {
         //DEEPLY unsafe - change before production
-        return block.timestamp;
+        return keccak256(block.timestamp);
+    }
+    
+    function rightShift() public view returns (bytes1, bytes1, bytes1) {
+        bytes32 test = generateRandomNumber();
+        return (test[0], bytes1(3*16), bytes1(test[0]) & bytes1(3*16));
     }
     function kill() private { 
         if (msg.sender == owner) 
