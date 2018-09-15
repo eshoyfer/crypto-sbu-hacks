5eda7c5316bacf3b2f9c932c92a84b834ce2b10c
omarhegazy <omarbicboi@gmail.com>
2018-09-15 09:02:54 -0400

    Still broken. Very broken.

--- dreidelGameOmar.sol
+++ dreidelGameOmar.sol
@@ -19,7 +19,8 @@ contract Dreidel {
 
     constructor(uint8 _playersUpper) public payable {
         owner = msg.sender;
-        playersUpper = _playersUpper;
+        //playersUpper = _playersUpper;
+        playersUpper = 2;
         playerCount = 1;
         stake = msg.value;
         players[0] = owner;
@@ -48,23 +49,26 @@ contract Dreidel {
     function computeGame() public view returns(uint8[]) {
         //come on Barbie let's go party
         require(playerCount > 1);
-        uint8[] storage pieceCount;
+        uint8[] memory pieceCount = new uint8[](playerCount);
         uint8 pot = 0;
-        for (uint i = 0; i < playerCount; i++)  pieceCount.push(PIECE_AMT);
+        for (uint i = 0; i < playerCount; i++)  pieceCount[i] = PIECE_AMT;
         bytes32 game = generateRandomNumber();
         uint8 playerIndex = 0;
+        uint8 livingPlayers = playerCount;
         uint8 bytesIndex = 0;
         bytes1 three = bytes1(3*16);
-        while (pieceCount.length > 1 && bytesIndex < 32) {
+        while (livingPlayers > 1 && bytesIndex < 32) {
+            while(pieceCount[playerIndex] == 0) 
+                playerIndex = (playerIndex + 1) % uint8(playerCount);
             bytes1 curr = game[bytesIndex];
             for(uint8 j = 0; j < 4; j++) {
                 bytes1 ourTurn = (curr & three);
                 curr = curr << 2;
                 if (ourTurn == 0) { //nun
-                    playerIndex = (playerIndex + 1) % uint8(pieceCount.length); 
+                    playerIndex = (playerIndex + 1) % uint8(playerCount); 
                 } else if (ourTurn == 1) { //gimel
                     pieceCount[playerIndex] += pot;
-                    playerIndex = (playerIndex + 1) % uint8(pieceCount.length);
+                    playerIndex = (playerIndex + 1) % uint8(playerCount);
                 } else if (ourTurn == 2) { //hei
                     pieceCount[playerIndex] += (pot + (pot % 2)) / 2;
                 } else if (ourTurn == 3) { //shin
@@ -73,7 +77,6 @@ contract Dreidel {
                         remove++; pieceCount[playerIndex]--;
                     }
                     pot+= remove;
-                    if (pieceCount[playerIndex] == 0) delete pieceCount[playerIndex];
                 }
             }
             bytesIndex++;
