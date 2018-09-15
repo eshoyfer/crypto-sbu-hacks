915a11d701e802ea12f7de9ce44b00a0e2175966
Elvin Shoyfer <eshoyfer@gmail.com>
2018-09-15 02:49:50 -0400

    Tweaks

--- dreidelGame.sol
+++ dreidelGame.sol
@@ -7,20 +7,22 @@ contract dreidelGame {
     uint8 id; // Unique
     uint8 players; // Starts at 1
     uint8 playersUpper; // Player defined 2-6
-    uint32 startTime; // Block
+    uint256 startTime; // Block
     uint8 status; // 0 = joinable; 1 = in progress; 2 = ended
-    uint8 stake; // Player defined stake per player; sent to join
+    uint stake; // Player defined stake per player; sent to join
 
     // Create
-    constructor(uint8 _stake, uint8 _playersUpper) public payable {
+    constructor(uint8 _playersUpper) public payable {
         owner = msg.sender;
-        stake = _stake;
         playersUpper = _playersUpper;
         players = 1;
+        stake = msg.value;
 
         if (playersUpper < 2 || playersUpper > 6) {
             kill();
         }
+
+        startTime = block.timestamp;
     }
 
     function kill() public {
