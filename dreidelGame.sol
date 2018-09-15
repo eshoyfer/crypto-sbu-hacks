bab5a83786fb5847c566d5b5bb7100e4a4c34c38
Elvin Shoyfer <eshoyfer@gmail.com>
2018-09-15 02:30:57 -0400

    Version change, constructor, kill

--- dreidelGame.sol
+++ dreidelGame.sol
@@ -1,8 +1,9 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.22;
 contract dreidelGame {
 
     //
 
+    address owner; // Creator
     uint id; // Unique
     uint players; // Starts at 1
     uint playersUpper; // Player defined 2-6
@@ -11,8 +12,14 @@ contract dreidelGame {
     uint stake; // Player defined stake per player; sent to join
 
     // Create
-    function dreidelGame(uint8 _stake) public {
-        // = msg.sender;
+    constructor(uint8 _stake, uint) public payable {
+        owner = msg.sender;
+    }
+
+    function kill() public {
+        if (msg.sender == owner) {
+            selfdestruct(owner);
+        }
     }
 
 }
